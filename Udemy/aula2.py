44a3084ba1c17eb91382ff2a6f5bebb6299c5085
Matheus Ribeiro Nunes <158311367+DevMrn@users.noreply.github.com>
2026-02-12 23:47:31 -0300

    Aula 2 e 3

--- Udemy/aula2.py
+++ Udemy/aula2.py
@@ -1,1 +1,10 @@
-#abc
+# \r é um return
+# n é um line feed
+# end= é para parar a quebra de linha
+'''
+sep='-' altera o espaço vazio entre os numeros
+        e põe o traço, ou alguma outra coisa.
+'''
+print(12, 34, 1011, sep="-")
+print(56, 78, sep='-')
+print(9, 10, sep='-')
